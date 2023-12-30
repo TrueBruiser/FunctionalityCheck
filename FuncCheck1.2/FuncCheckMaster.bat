ad1296a57ef7c935ae4f54ae573bba30f7348cf2
Happy Gilmore <98434426+TrueBruiser@users.noreply.github.com>
2023-12-29 21:44:20 -0500

    Update FuncCheckMaster.bat
    now using user input to run, rerun, or exit script entirely

--- FuncCheck1.2/FuncCheckMaster.bat
+++ FuncCheck1.2/FuncCheckMaster.bat
@@ -16,6 +16,28 @@ if %progress% leq 50 (
 endlocal
 goto :eof
 
+:: User input function
+:UserInput
+echo Press 'y' to continue to the next script, 'n' to rerun this step, 'e' to exit the script.
+set /p choice="Enter your choice: "
+if /i "%choice%"=="y" goto :Continue
+if /i "%choice%"=="n" goto :Rerun
+if /i "%choice%"=="e" goto :Exit
+echo Invalid choice. Please enter 'y', 'n', or 'e'.
+goto :UserInput
+
+:Continue
+echo Continuing to the next script...
+goto :eof
+
+:Rerun
+echo Rerunning the current script...
+goto :eof
+
+:Exit
+echo Exiting the script...
+exit
+
 :: Run the PowerShell script for Windows Update
 echo Running Windows Update script...
 start /b powershell -NoProfile -File "WinUpdate.ps1"
@@ -23,6 +45,7 @@ call :ShowProgress
 echo.
 echo Windows Update script completed.
 echo.
+call :UserInput
 
 :: Run the Batch script for System Test and Health
 echo Running System Test and Health script...
@@ -31,6 +54,7 @@ call :ShowProgress
 echo.
 echo System Test and Health script completed.
 echo.
+call :UserInput
 
 :: Open Screen Test HTML file
 echo Running Screen Test...
@@ -38,3 +62,4 @@ start "" "ScreenTest.html"
 echo.
 echo Screen Test will open in your default web browser.
 echo.
+call :UserInput
